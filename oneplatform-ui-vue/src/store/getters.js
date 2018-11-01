252f459e06e8c9bf241c3d094875270161749b1f
vakinge <vakinge@gmail.com>
2018-11-01 18:54:09 +0800

    +

--- oneplatform-ui-vue/src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
-  menus: state => state.user.menus
-}
-export default getters
